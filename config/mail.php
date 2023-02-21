9d171ebba27237761d4744d3097498d19ebe9b41
typhaney <typhaney@sandovaldesign.com>
2023-02-20 21:16:27 -0700

    local changes

--- config/mail.php
+++ config/mail.php
@@ -83,8 +83,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'typhaney@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Typhaney Bennett'),
     ],
 
     /*
